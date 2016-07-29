014a700f9d9aea961ec2df0075334a7e8662a323
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-07-29 19:07:55 +0200

    Automatically deploy changelog to master.

---
+++ update-changelog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! Changelog not updated!"
+  exit 0
+fi
+
+github_changelog_generator
+
+rev=$(git rev-parse --short HEAD)
+git add -A .
+git commit -m "updated changelog at ${rev}"
+git push -q upstream HEAD:gh-pages
