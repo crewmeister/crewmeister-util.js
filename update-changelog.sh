9dff60fba1de990241ddd65da8e6a4cfb1205d94
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-07-30 13:28:50 +0200

    Automatically generate and commit changelog when PRs get merged. (#2)
    * Added line breaks
    
    * Remove lines.
    
    * Use test changelog for pushing.
    
    * Test against changelog branch.
    
    * Verbose output.
    
    * Updated github token.
    
    * Checkout master branch.
    
    * Remove CHANGELOG.md
    
    * updated changelog at 60d219e
    
    * Remove changelog.
    
    * Make changelog branch configurable.
    
    * updated changelog at 963b66f
    
    * Change changelog branch to master.
    
    * Fix wrong if.
    
    * PRs don't create a new changelog.

--- update-changelog.sh
+++ update-changelog.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! Changelog not updated!"
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$CHANGELOG_BRANCH" ]; then
+  echo "This commit was made against the $TRAVIS_BRANCH and not $CHANGELOG_BRANCH! Changelog not updated!"
   exit 0
 fi
-
-github_changelog_generator
 
 rev=$(git rev-parse --short HEAD)
 
@@ -15,7 +12,10 @@ git config user.email "thomas@crewmeister.com"
 
 git remote add upstream "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 git fetch upstream
+git checkout $CHANGELOG_BRANCH
+
+github_changelog_generator
 
 git add -A .
 git commit -m "updated changelog at ${rev}"
-git push upstream master
+git push upstream $CHANGELOG_BRANCH
