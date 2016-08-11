5c9f81c5fc205e07f45a71e0acdfcb55a9eb6d4a
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-08-11 14:32:39 +0200

    Export some more.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import * as day from './datetime/day';
+import * as datetimeDay from './datetime/day';
+import * as datetimeMonth from './datetime/month';
+import * as datetimeYear from './datetime/year';
+import * as datetimeCalc from './datetime/calc';
+import * as datetimeFormat from './datetime/format';
 
 export const datetime = {
-  day
+  day: datetimeDay,
+  month: datetimeMonth,
+  year: datetimeYear,
+  format: datetimeFormat,
+  calc: datetimeCalc,
 };
